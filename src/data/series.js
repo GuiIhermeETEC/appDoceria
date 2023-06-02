7376c84dc4ab07e4ddfafb3ead8eb9517a95907e
unknown <you@example.com>
2023-06-02 17:13:55 -0300

    adicionando series

---
+++ src/data/series.js
@@ -0,0 +1,74 @@
+const DATA =
+[
+    {
+        id:"01",
+        nome:"O Mundo Sombrio de Sabrina",
+        notas:'10',
+        imagem:'caos.png'
+    },
+
+    {
+        id:"02",
+        nome:"iZombie",
+        notas:'9,0',
+        imagem:'izombie.jpg'
+    },
+
+    {
+        id:"03",
+        nome:"Once Upon a Time",
+        notas:'10',
+        imagem:'ouat.png'
+    },
+
+    {
+        id:"04",
+        nome:"Stranger Things",
+        notas:'9,5',
+        imagem:'strangerthings.jpg'
+    },
+
+    {
+        id:"05",
+        nome:"The 100",
+        notas:'9,0',
+        imagem:'the100.jpg'
+    },
+
+    {
+        id:"06",
+        nome:"The Big Bang Theory",
+        notas:'8,0',
+        imagem:'thebigbangtheory.jpg'
+    },
+
+    {
+        id:"07",
+        nome:"The Walking Dead",
+        notas:'8,5',
+        imagem:'twd.jpg'
+    },
+
+    {
+        id:"08",
+        nome:"Vikings",
+        notas:'10',
+        imagem:'vikings.jpg'
+    },
+
+    {
+        id:"09",
+        nome:"WandaVision",
+        notas:'10',
+        imagem:'wandavision.png'
+   },
+
+    {
+        id:"10",
+        nome:"Sweet Tooth",
+        notas:'10',
+        imagem:'swett.jpg'
+    }
+]
+
+export default DATA;
